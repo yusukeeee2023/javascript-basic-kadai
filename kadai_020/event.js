4827b2cc22543201d4daa3bcd32c19eee312d902
yusukeeee2023 <yusukeeee2023@gmail.com>
2024-09-14 13:06:39 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,13 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const clickBtn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const showText = document.getElementById('text');
+
+// クリックした時にイベントを実行する
+clickBtn.addEventListener('click', () => {
+  // 作成したtext要素に「ボタンがクリックされました」というテキストを追加する= text.content
+  showText.textContent ='ボタンがクリックされました';
+});
+
+
